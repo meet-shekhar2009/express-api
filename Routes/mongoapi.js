1846078e47e4e97d1ab774591d8f82445348dfa7
Chandra Shekhar <shekhar@arista.com>
2023-12-13 10:21:53 +0530

    log

--- Routes/mongoapi.js
+++ Routes/mongoapi.js
@@ -33,11 +33,12 @@ accountRoutes.post(`${baseUrl}/findone`, async (req, res) => {
 
 accountRoutes.get(`${baseUrl}/list`, async (req, res) => {
   let input = {};
-  console.log(req.headers.usersession);
+
   if (req.headers.usersession) {
     const { username } = JSON.parse(req.headers.usersession);
-    console.log('user', username);
+
     input = { ...input, username, ...getCurrentDate() };
+    console.log('input', input);
   }
   let include = {};
   if (req.headers.include) {
